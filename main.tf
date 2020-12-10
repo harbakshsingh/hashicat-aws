2affb169420d410a27aaee0451b0c46d1ca1ab25
harbaksh <harry_ieee@yahoo.co.in>
2020-12-10 08:55:26 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
 
-git pull
+
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = "DevOps"
---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/Harbaksh-training/s3-bucket/aws"
+  version = "1.17.0"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  bucket_prefix = "Harry"
+  versioning = {
+    enabled = true
+  }
+
+}
+ 
