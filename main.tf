d3800c31c1e6064ec4c467b6e850fe74d0c46e8b
root <root@p-z3yfch4nafhc-workstation.c.instruqt-prod.internal>
2020-12-10 08:43:54 +0000

    my-commit-after-updating-tags

--- main.tf
+++ main.tf
@@ -123,8 +123,12 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
+
+git pull
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "DevOps"
+    Billable = "true" 
   }
 }
 
