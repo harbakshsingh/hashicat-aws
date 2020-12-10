0bbe798c2405e6ae5a9e2514a1b565d32a4f1f81
harbaksh <harry_ieee@yahoo.co.in>
2020-12-10 09:16:34 +0000

    Added s3 bucket with unique name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/Harbaksh-training/s3-bucket/aws"
   version = "1.17.0"
-  bucket = "my-s3-bucket"
+  bucket = "my-genuinely-new-gs3-bucket"
   acl    = "private"
   #bucket_prefix = "Harry"
   versioning = {
